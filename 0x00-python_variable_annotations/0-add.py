9452edcb4bfd258e8d95034ab74202b032933dc4
Ultimate Kaycee <ultimatekaycee@outlook.com>
2024-08-18 19:47:42 -0700

    first task

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Module for first Task.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''Fuunction to add two floating-point numbers.
+    '''
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+readme file about python variable annotation
