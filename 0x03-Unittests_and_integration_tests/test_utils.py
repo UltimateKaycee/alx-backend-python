8af790ad8e49ff0f88e1a1c9d7b3b7ae78fa51a3
Ultimate Kaycee <ultimatekaycee@outlook.com>
2024-10-31 06:08:58 +0000

    utils before hibernate and break

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Testing module for the utils package.
+"""Module utils testing.
 """
 import unittest
 from typing import Dict, Tuple, Union
@@ -14,7 +14,7 @@ from utils import (
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """To test the `access_nested_map` function"""
+    """Class for testing the `access_nested_map` function."""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -26,7 +26,7 @@ class TestAccessNestedMap(unittest.TestCase):
             path: Tuple[str],
             expected: Union[Dict, int],
             ) -> None:
-        """To test the output of `access_nested_map`"""
+        """Function for `access_nested_map`'s output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -39,13 +39,13 @@ class TestAccessNestedMap(unittest.TestCase):
             path: Tuple[str],
             exception: Exception,
             ) -> None:
-        """To test exception raising for `access_nested_map`"""
+        """Function for `access_nested_map`'s for raising exception."""
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
-    """To test the function `get_json`"""
+    """Class for the `get_json` function."""
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
@@ -55,7 +55,7 @@ class TestGetJson(unittest.TestCase):
             test_url: str,
             test_payload: Dict,
             ) -> None:
-        """Testing `get_json`'s output."""
+        """Function for testing `get_json`'s output."""
         attrs = {'json.return_value': test_payload}
         with patch("requests.get", return_value=Mock(**attrs)) as req_get:
             self.assertEqual(get_json(test_url), test_payload)
@@ -63,9 +63,9 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    """Testing the function `memoize`"""
+    """Class for testing `memoize` function."""
     def test_memoize(self) -> None:
-        """Testing the output `memoize`"""
+        """Function for `memoize`'s output."""
         class TestClass:
             def a_method(self):
                 return 42
